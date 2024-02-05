22e967a3d9df85d7de54e42cfa9cb83cc580ebcb
Eric Silva <ericfelipedev@gmail.com>
2024-02-04 21:46:53 -0300

    script

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+const key = "e9ca2a3d5c740c210cbfbfe3986f4958"
+
+function searchCity() {
+
+}
+
+function buttonInput() {
+    const cidade = document.querySelector(".input-cid").value
+
+    searchCity(cidade)
+}
